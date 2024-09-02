8a801472870140bda1a044559a06418e90d886df
Julia Matte De Carli <84540425+jup1n@users.noreply.github.com>
2024-09-02 20:04:34 -0300

    second commit

--- IA.py
+++ IA.py
@@ -4,11 +4,17 @@ class DnDCharacterCreator:
         return input().strip().lower()
     
     def define_gender(self):
+        print("Responda das sequintes perguntas com a sua letra de identificação (a/b/c)")
         print("O seu personagem se define com o genero: ")
-        question = "a) Feminino\n b) Masculino\n c) Indefinido"
+        question = "a) Feminino\nb) Masculino\nc) Indefinido"
         answer = self.ask_question(question)
         
-        gender = answer
+        if answer == "a":
+            gender = "Feminino"
+        elif answer == "b":
+            gender = "Masculino"
+        elif answer == "c":
+            gender = "Indefinido"
         
         return gender
         
@@ -47,7 +53,7 @@ class DnDCharacterCreator:
 
         return alignment
     
-    def define_race(self):
+    def define_calss(self):
         print("Responda as seguintes questões com 'sim' ou 'não'")
         question_melee = "Seu personagem é voltado para combate corpo a corpo?"
         answer_melee = self.ask_question(question_melee) == "sim"
@@ -98,9 +104,78 @@ class DnDCharacterCreator:
             return "Classe indefinida. Respostas não coincidem com uma classe específica."
 
         
+    def definir_raca(self):
+        print("Responda as seguintes questões com 'sim' ou 'não'")
+
+        question_forca = "Seu personagem tem aumento de força?"
+        answer_forca = self.ask_question(question_forca) == "sim"
+        
+        question_destreza = "Seu personagem tem aumento de destreza?"
+        answer_destreza = self.ask_question(question_destreza) == "sim"
+        
+        question_constituicao = "Seu personagem tem aumento de constituição?"
+        answer_constituicao = self.ask_question(question_constituicao) == "sim"
+        
+        question_inteligencia = "Seu personagem tem aumento de inteligência?"
+        answer_inteligencia = self.ask_question(question_inteligencia) == "sim"
+        
+        question_sabedoria = "Seu personagem tem aumento de sabedoria?"
+        answer_sabedoria = self.ask_question(question_sabedoria) == "sim"
+        
+        question_carisma = "Seu personagem tem aumento de carisma?"
+        answer_carisma = self.ask_question(question_carisma) == "sim"
+        
+        question_resistencia = "Seu personagem tem resistência a dano?"
+        answer_resistencia = self.ask_question(question_resistencia) == "sim"
+        
+        question_velocidade = "Seu personagem tem velocidade base de 30 pés?"
+        answer_velocidade = self.ask_question(question_velocidade) == "sim"
+        
+        question_armas = "Seu personagem possui proficiência com armas?"
+        answer_armas = self.ask_question(question_armas) == "sim"
+        
+        question_idiomas = "Seu personagem sabe idiomas adicionais?"
+        answer_idiomas = self.ask_question(question_idiomas) == "sim"
+        
+        question_habilidades = "Seu personagem possui habilidades únicas?"
+        answer_habilidades = self.ask_question(question_habilidades) == "sim"
+        
+        question_tamanho_pequeno = "Seu personagem tem tamanho pequeno?"
+        answer_tamanho_pequeno = self.ask_question(question_tamanho_pequeno) == "sim"
+        
+        question_visao_escuro = "Seu personagem possui visão no escuro?"
+        answer_visao_escuro = self.ask_question(question_visao_escuro) == "sim"
+
+        if answer_forca and answer_destreza and answer_constituicao and not answer_inteligencia and not answer_sabedoria and answer_carisma and not answer_resistencia and answer_velocidade and not answer_armas and answer_idiomas and not answer_habilidades and not answer_tamanho_pequeno and not answer_visao_escuro:
+            return "Humano"
+        elif not answer_forca and not answer_destreza and answer_constituicao and not answer_inteligencia and answer_sabedoria and not answer_carisma and answer_resistencia and answer_velocidade and answer_armas and answer_idiomas and not answer_habilidades and not answer_tamanho_pequeno and answer_visao_escuro:
+            return "Anão"
+        elif not answer_forca and answer_destreza and not answer_constituicao and answer_inteligencia and answer_sabedoria and not answer_carisma and not answer_resistencia and answer_velocidade and answer_armas and not answer_idiomas and not answer_habilidades and not answer_tamanho_pequeno and answer_visao_escuro:
+            return "Elfo"
+        elif not answer_forca and answer_destreza and not answer_constituicao and not answer_inteligencia and not answer_sabedoria and answer_carisma and not answer_resistencia and not answer_velocidade and answer_armas and not answer_idiomas and not answer_habilidades and answer_tamanho_pequeno and not answer_visao_escuro:
+            return "Halfling"
+        elif answer_forca and not answer_destreza and answer_constituicao and not answer_inteligencia and not answer_sabedoria and not answer_carisma and not answer_resistencia and answer_velocidade and answer_armas and not answer_idiomas and not answer_habilidades and not answer_tamanho_pequeno and answer_visao_escuro:
+            return "Meio-Orc"
+        elif not answer_forca and answer_destreza and not answer_constituicao and answer_inteligencia and not answer_sabedoria and not answer_carisma and not answer_resistencia and not answer_velocidade and not answer_armas and answer_idiomas and answer_habilidades and answer_tamanho_pequeno and answer_visao_escuro:
+            return "Gnomo"
+        elif not answer_forca and answer_destreza and not answer_constituicao and not answer_inteligencia and answer_sabedoria and answer_carisma and not answer_resistencia and answer_velocidade and answer_armas and not answer_idiomas and answer_habilidades and not answer_tamanho_pequeno and answer_visao_escuro:
+            return "Meio-Elfo"
+        elif answer_forca and not answer_destreza and answer_constituicao and not answer_inteligencia and not answer_sabedoria and answer_carisma and not answer_resistencia and answer_velocidade and answer_armas and not answer_idiomas and answer_habilidades and not answer_tamanho_pequeno and answer_visao_escuro:
+            return "Draconato"
+        else:
+            return "Raça indefinida. Respostas não coincidem com uma raça específica."
+
+        
     def create_character(self):
+        gender = self.define_gender()
         alignment = self.define_alignment()
+        race = self.define_race()
+        classe = self.define_call()
+        
+        print(f"Genero: {gender}")
         print(f"Alinhamento: {alignment}")
+        print(f"Raça: {race}")
+        print(f"Classe: {classe}")
 
 # Uso da classe
 creator = DnDCharacterCreator()
